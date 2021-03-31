bc2f30d50e154675c39755ca6acd85d17941a759
Pribelszki Levente <pribelszkilevi@gmail.com>
2021-03-31 02:25:43 +0200

    Move CountryListActivity to its own file

--- app/src/main/java/hu/bme/aut/pribelszki/covidio/screen/country/list/CountryListActivity.kt
+++ app/src/main/java/hu/bme/aut/pribelszki/covidio/screen/country/list/CountryListActivity.kt
@@ -1,7 +1,8 @@
-package hu.bme.aut.pribelszki.covidio
+package hu.bme.aut.pribelszki.covidio.screen.country.list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hu.bme.aut.pribelszki.covidio.R
 
 class CountryListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
