3a46034f2a37347795df4941a78099de75a8f719
Pribelszki Levente <pribelszkilevi@gmail.com>
2021-03-31 02:08:36 +0200

    Rename MainActivity to CountryListActivity

--- app/src/main/java/hu/bme/aut/pribelszki/covidio/CountryListActivity.kt
+++ app/src/main/java/hu/bme/aut/pribelszki/covidio/CountryListActivity.kt
@@ -3,9 +3,9 @@ package hu.bme.aut.pribelszki.covidio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CountryListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_country_list)
     }
 }
