7b75a6650755910de0cf9098ffb7bdc1c3975ee2
Pribelszki Levente <pribelszkilevi@gmail.com>
2021-05-04 11:30:28 +0200

    Create TabLayout

--- app/src/main/java/hu/bme/aut/pribelszki/covidio/screen/country/details/CountryDetailsActivity.kt
+++ app/src/main/java/hu/bme/aut/pribelszki/covidio/screen/country/details/CountryDetailsActivity.kt
@@ -2,11 +2,42 @@ package hu.bme.aut.pribelszki.covidio.screen.country.details
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import hu.bme.aut.pribelszki.covidio.R
+import hu.bme.aut.pribelszki.covidio.screen.country.details.adapter.ViewPagerAdapter
 
 class CountryDetailsActivity : AppCompatActivity() {
+    var tabLayout: TabLayout? = null
+    var viewPager: ViewPager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_country_details)
+        setupTabLayout()
+    }
+
+    private fun setupTabLayout() {
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager = findViewById(R.id.viewPager)
+
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Home"))
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Sport"))
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Movie"))
+        tabLayout!!.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter = ViewPagerAdapter(supportFragmentManager, tabLayout!!.tabCount)
+        viewPager!!.adapter = adapter
+
+        viewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+
+        tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager!!.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
     }
 }
+
